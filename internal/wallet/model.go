219be36827c20c821df0323e6a64e0c711cda950
Chandrakant <chandrakantshinde7348@gmail.com>
2026-05-22 22:21:41 +0530

    feat: add wallet, transfer and LedgerEntry models

--- internal/wallet/model.go
+++ internal/wallet/model.go
@@ -1,10 +1,53 @@
 package wallet
 
+import "time"
+
+type TransactionStateType string
+type WalletStatusType string
+type LedgerEntryType string
+
+const (
+	StateProcessed TransactionStateType = "PROCESSED"
+	StatePending   TransactionStateType = "PENDING"
+	StateFailed    TransactionStateType = "FAILED"
+)
+
+const (
+	LedgerEntryCredit LedgerEntryType = "CREDIT"
+	LedgerEntryDebit  LedgerEntryType = "DEBIT"
+)
+
+const (
+	WalletStatusActive   WalletStatusType = "ACTIVE"
+	WalletStatusInActive WalletStatusType = "INACTIVE"
+)
+
 type Wallet struct {
+	WalletId  string           `db:"wallet_id"`
+	Balance   int64            `db:"balance"`
+	Status    WalletStatusType `db:"status"`
+	CreatedAt time.Time        `db:"created_at"`
+	UpdatedAt time.Time        `db:"updated_at"`
 }
 
 type Transfer struct {
+	TransactionId  string               `db:"transaction_id"`
+	IdempotencyKey string               `db:"idempotency_key"`
+	FromWalletId   string               `db:"from_wallet_id"`
+	ToWalletId     string               `db:"to_wallet_id"`
+	State          TransactionStateType `db:"status"`
+	CreatedAt      time.Time            `db:"created_at"`
+	UpdatedAt      time.Time            `db:"updated_at"`
 }
 
 type LedgerEntry struct {
+	TransactionId string          `db:"transaction_id"`
+	EntryId       string          `db:"entry_id"`
+	WalletId      string          `db:"wallet_id"`
+	Amount        int64           `db:"amount"`
+	Entry         LedgerEntryType `db:"entry"`
+	BalanceBefore int64           `db:"balance_before"`
+	BalanceAfter  int64           `db:"balance_after"`
+	CreatedAt     time.Time       `db:"created_at"`
+	UpdatedAt     time.Time       `db:"updated_at"`
 }
